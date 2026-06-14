f8db1a0a282ce45bd065ac819a928523c86ea455
Daniel E Gonzalez <dgonxalex80@gmail.com>
2026-06-14 14:30:19 -0500

    actualizacion jun 14-14-30

--- js/pages/goleadores.js
+++ js/pages/goleadores.js
@@ -1,5 +1,12 @@
 function renderScorers() {
-  $("#scorersRows").innerHTML = scorers.map((row) => `
+  const orderedScorers = [...scorers].sort((a, b) =>
+    b[2] - a[2] ||
+    b[3] - a[3] ||
+    a[4] - b[4] ||
+    a[0].localeCompare(b[0])
+  );
+
+  $("#scorersRows").innerHTML = orderedScorers.map((row) => `
     <tr><td data-label="Jugador"><strong>${row[0]}</strong></td><td data-label="Equipo">${teamLabel(row[1])}</td><td data-label="Goles">${row[2]}</td><td data-label="Asistencias">${row[3]}</td><td data-label="Minutos">${row[4]}</td></tr>
   `).join("");
 }

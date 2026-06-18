f0c58657273ad4c9a46088996a7b082093349ff6
Daniel E Gonzalez <dgonxalex80@gmail.com>
2026-06-17 20:57:00 -0500

    ajustes a 2026-06-20-57

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const groupStageMatches = [
   { id: 20, date: "2026-06-16 23:00", home: "Austria", away: "Jordania", phase: "Grupos", group: "J", stadium: "San Francisco Bay Area Stadium", city: "San Francisco Bay Area", status: "finalizado", homeScore: 3, awayScore: 1 },
   { id: 21, date: "2026-06-17 12:00", home: "Portugal", away: "Republica Democratica del Congo", phase: "Grupos", group: "K", stadium: "Houston Stadium", city: "Houston", status: "finalizado", homeScore: 1, awayScore: 1 },
   { id: 22, date: "2026-06-17 15:00", home: "Inglaterra", away: "Croacia", phase: "Grupos", group: "L", stadium: "Dallas Stadium", city: "Dallas", status: "finalizado", homeScore: 4, awayScore: 2 },
-  { id: 23, date: "2026-06-17 18:00", home: "Ghana", away: "Panama", phase: "Grupos", group: "L", stadium: "Toronto Stadium", city: "Toronto", status: "pendiente" },
+  { id: 23, date: "2026-06-17 18:00", home: "Ghana", away: "Panama", phase: "Grupos", group: "L", stadium: "Toronto Stadium", city: "Toronto", status: "finalizado", homeScore: 1, awayScore: 0 },
   { id: 24, date: "2026-06-17 21:00", home: "Uzbekistan", away: "Colombia", phase: "Grupos", group: "K", stadium: "Estadio Ciudad de Mexico", city: "Ciudad de Mexico", status: "pendiente" },
   { id: 25, date: "2026-06-18 11:00", home: "Republica Checa", away: "Sudafrica", phase: "Grupos", group: "A", stadium: "Atlanta Stadium", city: "Atlanta", status: "pendiente" },
   { id: 26, date: "2026-06-18 14:00", home: "Suiza", away: "Bosnia y Herzegovina", phase: "Grupos", group: "B", stadium: "Los Angeles Stadium", city: "Los Angeles", status: "pendiente" },
@@ -212,7 +212,9 @@ const scorers = [
   ["Marcus Rashford", "Inglaterra", 1, 0, 90],
   ["Martin Baturina", "Croacia", 1, 0, 90],
   ["Petar Musa", "Croacia", 1, 0, 90],
-  ["Bukayo Saka", "Inglaterra", 0, 1, 90]
+  ["Bukayo Saka", "Inglaterra", 0, 1, 90],
+  ["Caleb Yirenkyi", "Ghana", 1, 0, 95],
+  ["Brandon Thomas-Asante", "Ghana", 0, 1, 95]
 ];
 
 const allTimeWorldCupScorers = [
